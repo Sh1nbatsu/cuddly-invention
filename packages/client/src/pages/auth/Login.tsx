9c76819e33eee6a6ca038d0077e46247541422c4
Kirill <marchenko@finleo.ru>
2025-05-05 21:50:52 +0300

    feat: create login page

--- packages/client/src/pages/auth/Login.tsx
+++ packages/client/src/pages/auth/Login.tsx
@@ -1,3 +1,76 @@
+import { FormInput } from '@/components/FormInput/FormInput'
+import Wrapper from '@/components/Wrapper'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { LoginFormData, LoginSchema } from './schemas'
+import {
+  AuthFooterText,
+  AuthForm,
+  AuthLink,
+  AuthSpace,
+  AuthSubmitButton,
+  AuthTitle,
+} from './styled'
+
+const DEFAULT_VALUES = {
+  email: '',
+  password: '',
+}
+
 export const Login = () => {
-  return <div>Login</div>
+  const {
+    handleSubmit,
+    control,
+    formState: { isValid },
+  } = useForm({
+    defaultValues: DEFAULT_VALUES,
+    mode: 'onChange',
+    resolver: zodResolver(LoginSchema),
+  })
+
+  const onSubmit = (data: LoginFormData) => {
+    console.log('Submitted data:', data)
+  }
+
+  const onFinish = () => {
+    return handleSubmit(onSubmit)()
+  }
+
+  return (
+    <Wrapper>
+      <AuthForm onFinish={onFinish} layout="vertical" autoComplete="off">
+        <AuthTitle level={2}>Вход</AuthTitle>
+        <FormInput
+          control={control}
+          name="email"
+          label="Почта"
+          inputProps={{
+            type: 'email',
+          }}
+        />
+        <FormInput
+          control={control}
+          name="password"
+          label="Пароль"
+          inputProps={{
+            type: 'password',
+            autoComplete: 'new-password',
+          }}
+        />
+        <AuthSubmitButton
+          variant="outlined"
+          htmlType="submit"
+          disabled={!isValid}
+          size="large">
+          Войти
+        </AuthSubmitButton>
+
+        <AuthSpace direction="horizontal">
+          <AuthFooterText>
+            Нет аккаунта? <AuthLink to="/sign-up">Зарегистрироваться</AuthLink>
+          </AuthFooterText>
+        </AuthSpace>
+      </AuthForm>
+    </Wrapper>
+  )
 }
--- packages/client/src/pages/auth/schemas.ts
+++ packages/client/src/pages/auth/schemas.ts
@@ -24,3 +24,19 @@ export const RegisterSchema = z.object({
 })
 
 export type RegisterFormData = z.infer<typeof RegisterSchema>
+
+export const LoginSchema = z.object({
+  email: z
+    .string()
+    .email('Введите корректный email')
+    .max(50, 'Email не должен превышать 50 символов'),
+
+  password: z
+    .string()
+    .min(6, 'Пароль должен содержать минимум 6 символов')
+    .max(30, 'Пароль не должен превышать 30 символов')
+    .regex(/[A-Z]/, 'Пароль должен содержать хотя бы одну заглавную букву')
+    .regex(/[0-9]/, 'Пароль должен содержать хотя бы одну цифру'),
+})
+
+export type LoginFormData = z.infer<typeof LoginSchema>
