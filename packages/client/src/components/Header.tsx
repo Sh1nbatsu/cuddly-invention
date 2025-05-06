cb8f5a8fe082a5226e1debedfa4e0790024da284
Kirill <marchenko@finleo.ru>
2025-05-06 22:00:54 +0300

    chore: cherry pick commits

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CustomLink from '@/components/CustomLink'
+import { CustomLink } from './CustomLink/CustomLink'
 
 interface HeaderProps {
   logo?: React.ReactNode
