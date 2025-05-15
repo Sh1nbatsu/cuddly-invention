358fedf9c70e31f3dd326381b1b2674ce1df3ba1
sh1nbatsu <oleg.brusnik.93@mail.ru>
2025-05-15 12:52:53 +0500

    feat: created store, slice, user getter

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router/router'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from '@/store/store'
 
 import '../global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ReduxProvider store={store}>
+      <RouterProvider router={router} />
+    </ReduxProvider>
   </React.StrictMode>
 )
