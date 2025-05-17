fbd57430031b822ea4f22d9f5a82cfbdb093ea03
sh1nbatsu <oleg.brusnik.93@mail.ru>
2025-05-17 12:50:36 +0500

    fix: conflicts

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,21 +1,25 @@
-import { router } from '@/router/router'
-import { ConfigProvider } from 'antd'
-import 'antd/dist/reset.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd'
+import { ThemeProvider } from 'styled-components'
 import { RouterProvider } from 'react-router-dom'
+
+import 'antd/dist/reset.css'
 import { defaultTheme } from './themes/defaultTheme'
+import { router } from './router/router'
+import '../global.css'
+
 import { Provider as ReduxProvider } from 'react-redux'
 import { store } from './store/store'
-
-import '../global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ReduxProvider store={store}>
     <ConfigProvider theme={defaultTheme}>
-      <React.StrictMode>
-        <RouterProvider router={router} />
-      </React.StrictMode>
+      <ThemeProvider theme={defaultTheme}>
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
+      </ThemeProvider>
     </ConfigProvider>
   </ReduxProvider>
 )
