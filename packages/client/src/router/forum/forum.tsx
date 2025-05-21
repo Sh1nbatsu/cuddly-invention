db949c29d3b5f8e9847c3b598da8061da5535455
Кирилл Витошнов <64968241+KirillVitoschnov@users.noreply.github.com>
2025-05-21 22:25:35 +0500

    feat: добавил страницы форума с мок данными

--- packages/client/src/router/forum/forum.tsx
+++ packages/client/src/router/forum/forum.tsx
@@ -1,16 +1,21 @@
+import React from 'react'
 import { ForumPage } from '@/pages/Forum/ForumPage'
 import { TopicPage } from '@/pages/Forum/TopicPage'
+import { ErrorPage } from '@/pages/ErrorPage/ErrorPage'
+import { PAGE_ERROR } from '@/config/errorConfig'
 
 export const forumRoutes = {
   path: '/',
   children: [
     {
-      path: '/forum',
+      path: 'forum',
       element: <ForumPage />,
+      errorElement: <ErrorPage config={PAGE_ERROR} />,
     },
     {
-      path: '/forum/:topicId',
+      path: 'forum/:topicId',
       element: <TopicPage />,
+      errorElement: <ErrorPage config={PAGE_ERROR} />,
     },
   ],
 }
