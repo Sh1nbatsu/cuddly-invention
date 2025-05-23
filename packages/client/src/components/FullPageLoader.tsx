3ce0b43164807389cbd79a0df3b2a526484f496f
Кирилл Витошнов <64968241+KirillVitoschnov@users.noreply.github.com>
2025-05-24 00:45:59 +0500

    fix:  правки после код ревью

--- packages/client/src/components/FullPageLoader.tsx
+++ packages/client/src/components/FullPageLoader.tsx
@@ -1,13 +1,15 @@
 import { Spin } from 'antd'
+import styled from 'styled-components'
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`
 
 export const FullPageLoader = () => (
-  <div
-    style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-    }}>
+  <LoaderWrapper>
     <Spin size="large" />
-  </div>
+  </LoaderWrapper>
 )
