7422b559aedbe56aef02417ae90ed1c66a6a9a5a
Attato <rde8519@mail.ru>
2025-05-26 20:07:56 +0300

    исправил косяки в роутере и временно убрал ProtectedRoute у HomePage

--- packages/client/src/router/router.tsx
+++ packages/client/src/router/router.tsx
@@ -2,38 +2,22 @@ import { createBrowserRouter } from 'react-router-dom'
 import { ProtectedRoute } from '@/components/ProtectedRoute'
 import HomePage from '@/pages/game/HomePage'
 import { ErrorPage } from '@/pages/ErrorPage/ErrorPage'
-import { Login } from '@/pages/auth/Login'
-import { Registration } from '@/pages/auth/Registration'
 import Leaderboard from '@/pages/game/leaderboard/LeaderboardPage'
 
-import { authRoutes } from './auth/auth'
+import { authRoutes } from '@/router/auth/auth'
 import { forumRoutes } from '@/router/forum/forum'
 import { PAGE_ERROR, NOT_FOUND_ERROR } from '@/config/errorConfig'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: (
-      <ProtectedRoute>
-        <HomePage />
-      </ProtectedRoute>
-    ),
+    element: <HomePage />,
     errorElement: <ErrorPage config={PAGE_ERROR} />,
   },
   authRoutes,
   forumRoutes,
   {
-    path: '/sign-up',
-    element: <Registration />,
-    errorElement: <ErrorPage config={PAGE_ERROR} />,
-  },
-  {
-    path: '/sign-in',
-    element: <Login />,
-    errorElement: <ErrorPage config={PAGE_ERROR} />,
-  },
-  {
-    path: '/game/leaderboard',
+    path: 'leaderboard',
     element: (
       <ProtectedRoute>
         <Leaderboard />
