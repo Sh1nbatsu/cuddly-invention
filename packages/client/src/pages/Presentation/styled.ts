00a7c364beef910db664495ed62f255df6e68aa3
tzarbasil <atreides03@mail.ru>
2025-05-30 16:06:35 +0200

    added landing page

---
+++ packages/client/src/pages/Presentation/styled.ts
@@ -0,0 +1,48 @@
+import { Typography } from 'antd'
+import styled from 'styled-components'
+
+export const StyledPresentationTitle = styled(Typography.Title)`
+  && {
+    font-family: 'PressStart2P', monospace;
+    font-size: 50px;
+    margin-bottom: 0;
+  }
+`
+
+export const StyledPresentationSubtitle = styled(Typography.Title)`
+  && {
+    font-size: 24px;
+    margin-top: 0;
+    margin-bottom: 30px;
+  }
+
+  span {
+    color: #f90808;
+  }
+`
+
+export const StyledPresentationList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin-bottom: 50px;
+  text-align: start;
+`
+
+export const StyledPresentationListElement = styled.li`
+  font-size: 20px;
+  padding-left: 5px;
+  list-style-type: disc;
+
+  &::marker {
+    content: '➤ '; // любой символ, например emoji
+    font-size: 12px;
+  }
+`
+
+export const StyledPresentationText = styled(Typography.Text)`
+  && {
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+`
