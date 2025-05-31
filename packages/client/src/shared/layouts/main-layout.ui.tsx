8c619fae7b45d3aaec2c58d20567bd19cc752afc
Kirill <marchenko.kiri11@yandex.ru>
2025-05-31 04:05:46 +0300

    fix: naiming folder

--- packages/client/src/shared/layouts/main-layout.ui.tsx
+++ packages/client/src/shared/layouts/main-layout.ui.tsx
@@ -1,6 +1,6 @@
 import { PageWrapper } from '@/shared/ui/page-wrapper/page-wrapper.ui'
 import { Outlet } from 'react-router-dom'
-import { Header } from '../ui/Header/Header'
+import { Header } from '../ui/header/header.ui'
 
 export const MainLayout = () => (
   <PageWrapper>
