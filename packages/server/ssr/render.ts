ae51fd54da6779b6a5fd2da59f1bc419a1ffe294
Kirill <marchenko.kiri11@yandex.ru>
2025-06-11 00:15:02 +0300

    wip:

--- packages/server/ssr/render.ts
+++ packages/server/ssr/render.ts
@@ -2,52 +2,72 @@ import express, { NextFunction, Request, Response } from 'express'
 import fs from 'fs/promises'
 import path from 'path'
 import { HelmetData } from 'react-helmet'
-import { createServer } from 'vite'
-
-// Типизации для функции packages/client/src/entries/entry-server.tsx
-type RenderFn = (req: Request) => Promise<{
-  appHtml: string
-  helmet: HelmetData
-  styleTags: string
-}>
+import { createServer, ViteDevServer } from 'vite'
 
 export async function setupSSR(app: express.Express, clientPath: string) {
-  const vite = await createServer({
-    server: { middlewareMode: true },
-    root: clientPath,
-    appType: 'custom',
-  })
+  const isDev = process.env.NODE_ENV === 'development'
+  let vite: ViteDevServer | undefined
+  if (isDev) {
+    vite = await createServer({
+      server: { middlewareMode: true },
+      root: clientPath,
+      appType: 'custom',
+    })
 
-  app.use(vite.middlewares)
+    app.use(vite.middlewares)
+  } else {
+    app.get('*', async (req: Request, res: Response, next: NextFunction) => {
+      const url = req.originalUrl
 
-  app.get('*', async (req: Request, res: Response, next: NextFunction) => {
-    const url = req.originalUrl
+      try {
+        let template: string
+        // Типизации для функции packages/client/src/entries/entry-server.tsx
+        let render: () => Promise<{
+          appHtml: string
+          styleTags: string
+          helmet: HelmetData
+        }>
+        if (vite) {
+          template = await fs.readFile(
+            path.resolve(clientPath, 'index.html'),
+            'utf-8'
+          )
 
-    try {
-      let template = await fs.readFile(
-        path.resolve(clientPath, 'index.html'),
-        'utf-8'
-      )
+          template = await vite.transformIndexHtml(url, template)
 
-      template = await vite.transformIndexHtml(url, template)
+          render = (
+            await vite.ssrLoadModule(
+              path.join(clientPath, 'src/entry-server.tsx')
+            )
+          ).render
+        } else {
+          template = await fs.readFile(
+            path.join(clientPath, 'dist/client/index.html'),
+            'utf-8'
+          )
 
-      const mod = (await vite.ssrLoadModule(
-        path.join(clientPath, 'src/entries/entry-server.tsx')
-      )) as { render: RenderFn }
+          const pathToServer = path.join(
+            clientPath,
+            'dist/server/entry-server.js'
+          )
 
-      const { appHtml, styleTags, helmet } = await mod.render(req)
+          render = (await import(pathToServer)).render
+        }
 
-      const html = template
-        .replace(`<!--ssr-outlet-->`, appHtml)
-        .replace('<!--styles-->', styleTags)
-        .replace('<!--helmet-title-->', helmet.title.toString())
-        .replace('<!--helmet-meta-->', helmet.meta.toString())
-        .replace('<!--helmet-link-->', helmet.link.toString())
+        const { appHtml, styleTags, helmet } = await render()
 
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
-    } catch (e) {
-      vite.ssrFixStacktrace(e as Error)
-      next(e)
-    }
-  })
+        const html = template
+          .replace(`<!--ssr-outlet-->`, appHtml)
+          .replace('<!--styles-->', styleTags)
+          .replace('<!--helmet-title-->', helmet.title.toString())
+          .replace('<!--helmet-meta-->', helmet.meta.toString())
+          .replace('<!--helmet-link-->', helmet.link.toString())
+
+        res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+      } catch (error) {
+        vite?.ssrFixStacktrace(error as Error)
+        next(error)
+      }
+    })
+  }
 }
