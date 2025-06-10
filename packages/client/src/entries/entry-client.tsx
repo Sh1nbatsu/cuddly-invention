92a76a8d751b61addcc44e671e59d35522fa5f88
Kirill <marchenko.kiri11@yandex.ru>
2025-06-10 23:36:02 +0300

    feat: update entry-client

--- packages/client/src/entries/entry-client.tsx
+++ packages/client/src/entries/entry-client.tsx
@@ -1,18 +1,31 @@
 import { routerConfig } from '@/providers/router/router.config'
+import { store } from '@/providers/store/store'
 import { antdTheme, styledTheme } from '@/shared/themes/defaultTheme'
 import { ConfigProvider } from 'antd'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider as ReduxProvider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+
+import 'antd/dist/reset.css'
+
+import { registerServiceWorker } from '@/sw/register'
+import '../shared/assets/global.css'
+
+if (import.meta.env.PROD) {
+  registerServiceWorker()
+}
 
 ReactDOM.hydrateRoot(
   document.getElementById('root')!,
   <React.StrictMode>
-    <ConfigProvider theme={antdTheme}>
-      <ThemeProvider theme={styledTheme}>
-        <RouterProvider router={routerConfig} />
-      </ThemeProvider>
-    </ConfigProvider>
+    <ReduxProvider store={store}>
+      <ConfigProvider theme={antdTheme}>
+        <ThemeProvider theme={styledTheme}>
+          <RouterProvider router={routerConfig} />
+        </ThemeProvider>
+      </ConfigProvider>
+    </ReduxProvider>
   </React.StrictMode>
 )
