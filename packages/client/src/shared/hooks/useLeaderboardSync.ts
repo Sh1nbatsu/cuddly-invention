c91f7fcc4faafcc9e293ab0c073008745ab5517a
sh1nbatsu <oleg.brusnik.93@mail.ru>
2025-06-15 22:44:43 +0500

    final logic done

--- packages/client/src/shared/hooks/useLeaderboardSync.ts
+++ packages/client/src/shared/hooks/useLeaderboardSync.ts
@@ -2,15 +2,18 @@ import { useEffect } from 'react'
 import { useScore } from '@/entities/game/model/hooks/useScore'
 import { useSelector } from 'react-redux'
 import { selectUser } from '@/entities/user/model/user.selector'
+import { useLocation } from 'react-router-dom'
+import { addLeaderHandler } from '@/entities/leaderboard/leaderboard.handler'
 
 const URL = 'https://ya-praktikum.tech/api/v2/leaderboard'
 
 export const useLeaderboardSync = () => {
   const user = useSelector(selectUser)
   const username = user.user?.login || 'undefined'
+  const [score] = useScore()
+  const location = useLocation()
 
   const syncLeaderboard = () => {
-    const [score] = useScore()
     const data = {
       data: {
         undefScore12: score,
@@ -23,6 +26,10 @@ export const useLeaderboardSync = () => {
     }
     navigator.sendBeacon(URL, JSON.stringify(data))
   }
+
+  useEffect(() => {
+    addLeaderHandler(score, username)
+  }, [location])
 
   useEffect(() => {
     window.addEventListener('beforeunload', syncLeaderboard)
