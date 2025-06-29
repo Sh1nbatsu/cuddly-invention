4435acb162d114358b9bc583a25ed8a7cd42a027
Kirill <marchenko.kiri11@yandex.ru>
2025-06-29 19:44:04 +0300

    chore: small fix for env and etc

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,27 @@
-import express from 'express'
+import express, { Router } from 'express'
 import path from 'path'
 import { connectDB } from './api/db/db'
+import sessionRouter from './api/routes/session.route'
 import { setupSSR } from './ssr/render'
 
-const PORT = 3000
 const CLIENT_PATH = path.resolve('../client')
+const PORT = process.env.SERVER_PORT
 
 async function startServer() {
   const app = express()
+  const router = Router()
 
   await connectDB()
   await setupSSR(app, CLIENT_PATH)
+  app.use(express.json())
 
+  router.use('/auth', sessionRouter)
+
+  app.use(router)
   app.listen(PORT, () => {
-    console.log(`  ➜ 🎸 Server is listening on port: ${PORT}`)
+    console.log(
+      `  ➜ 🎸 Server is listening on port: ${process.env.SERVER_PORT}`
+    )
   })
 }
 
