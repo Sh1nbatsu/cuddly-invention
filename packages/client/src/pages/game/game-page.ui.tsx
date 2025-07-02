16e2db79b5ecc2bc7bffeacc03ea3eb1369c2254
sh1nbatsu <oleg.brusnik.93@mail.ru>
2025-07-02 17:32:43 +0300

    game page conflict

--- packages/client/src/pages/game/game-page.ui.tsx
+++ packages/client/src/pages/game/game-page.ui.tsx
@@ -1,4 +1,5 @@
 import { GameProvider } from '@/entities/game/game.context'
+import { selectUser } from '@/entities/user/model/user.selector'
 import { fetchMe } from '@/entities/user/model/user.thunk'
 import { useAppDispatch, useAppSelector } from '@/providers/store/store.hooks'
 import { useLeaderboardSync } from '@/shared/hooks/useLeaderboardSync'
@@ -8,7 +9,7 @@ import { useEffect } from 'react'
 
 export const GamePage = () => {
   const dispatch = useAppDispatch()
-  const user = useCurrentUser()
+  const user = useAppSelector(selectUser)
   useEffect(() => {
     if (!user?.id) {
       dispatch(fetchMe())
