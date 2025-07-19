778d48dcb94fc1bd0326d1c7dd6adfd8b8ecbbf2
Кирилл Витошнов <64968241+KirillVitoschnov@users.noreply.github.com>
2025-07-19 18:23:53 +0500

    fix: доработки по оформлению

--- packages/client/src/shared/ui/auth/yandex-login-button.ui.tsx
+++ packages/client/src/shared/ui/auth/yandex-login-button.ui.tsx
@@ -1,14 +1,8 @@
 import { memo } from 'react'
-import styled from 'styled-components'
 import { AuthSubmitButton } from '@/entities/session/session.styled'
 
 const CLIENT_ID = import.meta.env.VITE_YANDEX_CLIENT_ID as string
 const ORIGIN = window.location.origin
-
-const StyledAuthButton = styled(AuthSubmitButton)`
-  margin-top: 16px;
-  margin-bottom: 16px;
-`
 
 export const YandexLoginButton = memo(() => {
   function handleClick(): void {
@@ -23,12 +17,12 @@ export const YandexLoginButton = memo(() => {
   }
 
   return (
-    <StyledAuthButton
+    <AuthSubmitButton
       variant="outlined"
       htmlType="button"
       size="large"
       onClick={handleClick}>
       Войти через Яндекс
-    </StyledAuthButton>
+    </AuthSubmitButton>
   )
 })
