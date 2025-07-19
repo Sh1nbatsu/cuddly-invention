e0e6e707ac21da9daa2bc18c19b8d629cac2c8a1
Кирилл Витошнов <64968241+KirillVitoschnov@users.noreply.github.com>
2025-07-19 18:48:59 +0500

    fix: доработки по оформлению

--- packages/client/src/shared/layouts/error/error.styled.ts
+++ packages/client/src/shared/layouts/error/error.styled.ts
@@ -9,11 +9,9 @@ export const PageContent = styled.div`
   height: calc(100vh - 148px);
   max-width: 50%;
   text-align: center;
-  padding: 2rem;
-  background: ${({ theme }) => theme.colors.backgroundBase};
   border-radius: 4px;
-  border: 2px solid ${({ theme }) => theme.colors.backgroundPrimary};
-  box-shadow: 4px 4px ${({ theme }) => theme.colors.backgroundPrimary};
+  border: 2px solid;
+  box-shadow: 4px 4px;
   font-weight: 600;
   margin: 32px auto auto;
   flex-direction: column;
