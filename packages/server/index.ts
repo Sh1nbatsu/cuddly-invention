8b71c0bf3a8151e22800277eb64cfc9fe56cfe96
sh1nbatsu <oleg.brusnik.93@mail.ru>
2025-07-22 22:28:10 +0300

    wip

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -16,9 +16,20 @@ async function startServer() {
 
   await connectDB()
 
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://titleisundefined.ya-praktikum.tech',
+  ]
+
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+      },
       credentials: true,
     })
   )
