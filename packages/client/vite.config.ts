bf19387db26735a62612ca05ecd2013160eb5094
sh1nbatsu <oleg.brusnik.93@mail.ru>
2025-07-23 20:02:33 +0300

    wip

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv'
 import path from 'path'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
-import ssr from 'vite-plugin-ssr/plugin'
 
 dotenv.config()
 
@@ -35,7 +34,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    ssr(),
     VitePWA({
       registerType: 'prompt',
       strategies: 'injectManifest',
