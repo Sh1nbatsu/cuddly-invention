0e6994f7015eefc689d015d0295e387601768d6b
Kirill <marchenko.kiri11@yandex.ru>
2025-07-31 18:35:59 +0300

    chore:

--- packages/client/src/sw/sw.ts
+++ packages/client/src/sw/sw.ts
@@ -27,25 +27,25 @@ swSelf.addEventListener('install', (event: ExtendableEvent) => {
   )
 })
 
-swSelf.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response
-      const fetchRequest = event.request.clone()
-      return fetch(fetchRequest).then(response => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response
-        }
-        const responseToCache = response.clone()
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, responseToCache)
-        })
+// swSelf.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request).then(response => {
+//       if (response) return response
+//       const fetchRequest = event.request.clone()
+//       return fetch(fetchRequest).then(response => {
+//         if (!response || response.status !== 200 || response.type !== 'basic') {
+//           return response
+//         }
+//         const responseToCache = response.clone()
+//         caches.open(CACHE_NAME).then(cache => {
+//           cache.put(event.request, responseToCache)
+//         })
 
-        return response
-      })
-    })
-  )
-})
+//         return response
+//       })
+//     })
+//   )
+// })
 
 swSelf.addEventListener('activate', (event: ExtendableEvent) => {
   event.waitUntil(
